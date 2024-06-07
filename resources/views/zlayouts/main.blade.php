b9d3c7cd6d5477474fbc7f1889d10b06cd1dc81f
Harris <harris.git21@gmail.com>
2024-06-07 14:11:43 +0700

    fix: change icon and cdn link

--- resources/views/zlayouts/main.blade.php
+++ resources/views/zlayouts/main.blade.php
@@ -14,7 +14,8 @@
     <link rel="stylesheet" href="./zlayouts/css/normalize.min.css">
     {{-- <link rel="stylesheet" href="./zlayouts/css/bootstrap.min.css"> --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="./zlayouts/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@icon/themify-icons@1.0.1-alpha.3/themify-icons.min.css">
     <link rel="stylesheet" href="./zlayouts/css/themify-icons.css">
     <link rel="stylesheet" href="./zlayouts/assets/css/cs-skin-elastic.css">
     <link rel="stylesheet" href="./zlayouts/assets/css/style.css">
@@ -25,36 +26,36 @@
     #weatherWidget .currentDesc {
         color: #ffffff!important;
     }
-        .traffic-chart {
-            min-height: 335px;
-        }
-        #flotPie1  {
-            height: 150px;
-        }
-        #flotPie1 td {
-            padding:3px;
-        }
-        #flotPie1 table {
-            top: 20px!important;
-            right: -10px!important;
-        }
-        .chart-container {
-            display: table;
-            min-width: 270px ;
-            text-align: left;
-            padding-top: 10px;
-            padding-bottom: 10px;
-        }
-        #flotLine5  {
-             height: 105px;
-        }
+    .traffic-chart {
+        min-height: 335px;
+    }
+    #flotPie1  {
+        height: 150px;
+    }
+    #flotPie1 td {
+        padding:3px;
+    }
+    #flotPie1 table {
+        top: 20px!important;
+        right: -10px!important;
+    }
+    .chart-container {
+        display: table;
+        min-width: 270px ;
+        text-align: left;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+    #flotLine5  {
+            height: 105px;
+    }
 
-        #flotBarChart {
-            height: 150px;
-        }
-        #cellPaiChart{
-            height: 160px;
-        }
+    #flotBarChart {
+        height: 150px;
+    }
+    #cellPaiChart{
+        height: 160px;
+    }
     </style>
 </head>
 
@@ -66,48 +67,54 @@
                 <ul class="nav navbar-nav">
                     <li class="menu-title">Main</li><!-- /.menu-title -->
                         <li class="@yield('activehome')">
-                            <a href="{{ url('/home') }}"><img src="./zlayouts/images/home.png" height="14px" alt="Main">&emsp;&emsp;Home </a>
+                            <a href="{{ url('/home') }}">
+                                <img src="./zlayouts/images/home.png"
+                                height="14px"
+                                alt="Main">
+                                Home
+                            </a>
                         </li>
                     </li>
-                    <li class="menu-title">Daily Transaction</li><!-- /.menu-title -->
+                    <li class="menu-title">Transaksi Harian</li><!-- /.menu-title -->
                         <li class="@yield('activeinput')">
-                            <a href="{{ url('/input') }}"><img src="./zlayouts/images/input.png" height="14px" alt="Pemasukkan">&emsp;&emsp; Pemasukkan</a>
+                            <a href="{{ url('/input') }}">
+                                <img src="./zlayouts/images/input.png"
+                                height="14px"
+                                alt="Pemasukkan" />
+                                Pemasukan
+                            </a>
                         </li>
                         <li class="@yield('activeoutput')">
-                            <a href="{{ url('/output') }}"><img src="./zlayouts/images/output.png" height="14px" alt="Pengeluaran">&emsp;&emsp; Pengeluaran</a>
+                            <a href="{{ url('/output') }}">
+                                <img src="./zlayouts/images/output.png"
+                                height="14px"
+                                alt="Pengeluaran" />
+                                Pengeluaran
+                            </a>
                         </li>
                     </li>
                     <li class="menu-title">Laporan Mutasi</li><!-- /.menu-title -->
-                        {{-- <li class="@yield('activegudangumum')">
-                            <a href="{{ url('/gudangumum') }}"><img src="./zlayouts/images/input.png" height="14px" alt="Gudang Umum">&emsp;&emsp; Gudang Umum</a>
+                        <li class="@yield('active_wip')">
+                            <a href="{{ url('/wip') }}">
+                                <img src="./zlayouts/images/wip.png"
+                                height="14px"
+                                alt="WIP" />
+                                WIP
+                            </a>
                         </li>
-                        <li class="@yield('activematerial')">
-                            <a href="{{ url('/material') }}"><img src="./zlayouts/images/input.png" height="14px" alt="Material">&emsp;&emsp; Gudang Material</a>
-                        </li>
-                        <li class="@yield('activefinishgood')">
-                            <a href="{{ url('/finishgood') }}"><img src="./zlayouts/images/input.png" height="14px" alt="Finish Good">&emsp;&emsp; Gudang Finished Goods</a>
-                        </li>
-                        <li class="@yield('activeproduction')">
-                            <a href="{{ url('/production') }}"><img src="./zlayouts/images/input.png" height="14px" alt="Production">&emsp;&emsp; Production</a>
-                        </li> --}}
                         <li class="@yield('active_bahan_baku')">
-                            <a href="{{ url('/bahan_baku') }}"><img src="./zlayouts/images/bahanbaku.png" height="14px" alt="Bahan Baku / Penolong">&emsp;&emsp; Bahan Baku / Penolong</a>
+                            <a href="{{ url('/bahan_baku') }}"><img src="./zlayouts/images/bahanbaku.png" height="14px" alt="Bahan Baku / Penolong">Bahan Baku / Penolong</a>
                         </li>
                         <li class="@yield('activefinishgood')">
-                            <a href="{{ url('/finishgood') }}"><img src="./zlayouts/images/finishgood.png" height="14px" alt="Finishgood">&emsp;&emsp; Barang Jadi</a>
+                            <a href="{{ url('/finishgood') }}"><img src="./zlayouts/images/finishgood.png" height="14px" alt="Finishgood">Barang Jadi</a>
                         </li>
                         <li class="@yield('active_mesin')">
-                            <a href="{{ url('/mesin') }}"><img src="./zlayouts/images/mesin.png" height="14px" alt="Mesin dan Peralatan">&emsp;&emsp; Mesin dan Peralatan</a>
+                            <a href="{{ url('/mesin') }}"><img src="./zlayouts/images/mesin.png" height="14px" alt="Mesin dan Peralatan">Mesin dan Peralatan</a>
                         </li>
                         <li class="@yield('activescrap')">
-                            <a href="{{ url('/scrap') }}"><img src="./zlayouts/images/scrap.png" height="14px" alt="Scrap">&emsp;&emsp; Barang Scrap</a>
+                            <a href="{{ url('/scrap') }}"><img src="./zlayouts/images/scrap.png" height="14px" alt="Scrap">Barang Scrap</a>
                         </li>
                     </li>
-                    {{-- <li class="menu-title">Setup</li><!-- /.menu-title -->
-                        <li class="@yield('activeuser')">
-                            <a href="#"><img src="./zlayouts/images/user.png" height="14px" alt="User">&emsp;&emsp;User </a>
-                        </li>
-                    </li> --}}
                 </ul>
             </div><!-- /.navbar-collapse -->
         </nav>
@@ -170,23 +177,11 @@
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-    {{-- <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="https://cdn.usebootstrap.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="https://cdn.usebootstrap.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script> --}}
-
-
-    {{-- <script src="../node_modules/jquery/dist/jquery.min.js"></script> --}}
-    {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> --}}
-    {{-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script> --}}
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> --}}
-    {{-- <script src="./zlayouts/js/popper.min.js"></script> --}}
-    {{-- <script src="./zlayouts/js/bootstrap.min.js"></script> --}}
-    <script src="./zlayouts/js/jquery.matchHeight.min.js"></script>
-    <script src="./zlayouts/assets/js/main.js"></script>
+    {{-- <script src="./zlayouts/js/jquery.matchHeight.min.js"></script>
+    <script src="./zlayouts/assets/js/main.js"></script> --}}
 
     @yield('stylejavascript')
 </body>
